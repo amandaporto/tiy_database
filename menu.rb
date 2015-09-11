d24fe3265f5cb2df30e512efd505c0725742cfe6
Gavin Stark <gavin@gstark.com>
2015-09-11 09:15:26 -0400

    Avoids downcasing names/github/slack
    We can still handle searching for names without consideration of case by modifying our new find_person_by_name method as shown

--- menu.rb
+++ menu.rb
@@ -30,7 +30,8 @@ class Menu
   end
 
   def find_person_by_name(name)
-    @people.find {|person| person.name.include?(name)}
+    downcase_name = name.downcase
+    @people.find {|person| person.name.downcase.include?(downcase_name)}
   end
 
   def add
@@ -39,7 +40,7 @@ class Menu
     if person_type == "s"
       puts "Name?"
       student_name = Student.new
-      student_name.name = gets.chomp.downcase
+      student_name.name = gets.chomp
       if find_person_by_name(student_name.name)
         puts "Student already exists"
       else
@@ -50,9 +51,9 @@ class Menu
         puts "Address?"
         student_name.address = gets.chomp
         puts "Github?"
-        student_name.github = gets.chomp.downcase
+        student_name.github = gets.chomp
         puts "Slack?"
-        student_name.slack = gets.chomp.downcase
+        student_name.slack = gets.chomp
         puts "Scholarships?"
         student_name.scholarship = gets.chomp
         puts "#{student_name.name} has been added as a student!"
@@ -61,7 +62,7 @@ class Menu
     elsif person_type == "e"
       puts "Name?"
       employee_name = Employee.new
-      employee_name.name = gets.chomp.downcase
+      employee_name.name = gets.chomp
       if find_person_by_name(employee_name.name)
         puts "Employee already exists"
       else
@@ -72,13 +73,13 @@ class Menu
         puts "Address?"
         employee_name.address = gets.chomp
         puts "Github?"
-        employee_name.github = gets.chomp.downcase
+        employee_name.github = gets.chomp
         puts "Slack?"
-        employee_name.slack = gets.chomp.downcase
+        employee_name.slack = gets.chomp
         puts "Salary?"
         employee_name.salary = gets.chomp
         puts "Position?"
-        employee_name.position = gets.chomp.downcase
+        employee_name.position = gets.chomp
         puts "Date Hired?"
         employee_name.date_hired = gets.chomp
         puts "#{employee_name.name} has been added as an employee!"
@@ -93,7 +94,7 @@ class Menu
 
   def search
     puts "Name to search?"
-    search_name = gets.chomp.downcase
+    search_name = gets.chomp
     found = find_person_by_name(search_name)
     if found == nil
       puts "#{search_name} is not in the database"
@@ -107,7 +108,7 @@ class Menu
 
   def delete
     puts "Name to delete?"
-    delete_name = gets.chomp.downcase
+    delete_name = gets.chomp
     deleted = find_person_by_name(delete_name)
     if deleted == nil
       puts "#{delete_name} is not in the database"
